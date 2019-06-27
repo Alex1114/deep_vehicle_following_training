123d1fcdf47273cb0480c439833bcb281276a321
Alex <pokai.eed05@g2.nctu.edu.tw>
2019-06-27 15:20:35 +0800

    first commit

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 # deep_vehicle_following_training
 Use fish camera to following car
+
+How to use the file
+1. run joystick_camera.launch or any launch which has the car_cmd_switch_node/cmd and camera_node/image/compressed topic on duckiebot
+2. use rosbag record command to record one or more bag files which only subscribe the two topics mentioned in 1. (recommend: use "--split --size 2048" parameter to split bag into multiple 2G bag files, cause the bag will corrupt if the size is too large)
+3. use bag2txt.py to draw out the image and omega value. For more info, see bag2txt.py
+4. put the folder with images into 'images' under 'for_ncs', and move the train.txt & test.txt to 'splits' folder under 'for_ncs'
+5. after setup the environment, run 'python train.py' to start the training, more info in the train.py
+6. after the training is done, there'll be a file called solvertemplate_iter_100000.caffemodel under for_ncs/results/0/snapshots/
+7. If you want to deploy the result to ncs, the rest process is the same as we taught in this summer school
---
+++ lanefollowing_twist/for_ncs/src/test_testset.py
@@ -0,0 +1,67 @@
+from subprocess import call
+import os
+from shutil import rmtree
+
+import numpy as np
+from settings import CAFFE_ROOT, GPU
+
+def evaluate_results(split):
+    # create the confusion matrix and link the misclassified images
+    call(["./CaffeNetAnalysis/CaffeNetAnalysisMain",
+          "../splits/test.txt",
+          os.path.join(os.path.abspath("../images/"), ""),
+          #os.path.join(os.path.abspath("../images/iros-2015-four-score/images/"), ""),    #for iros-2015 images
+          "../caffe_data/deploy.prototxt",
+          "../results/{0}/snapshots/solvertemplate_iter_10000.caffemodel".format(split),
+          "../classid.txt",
+          "../results/{0}/".format(split), str(GPU)])
+
+def export_np_mat_with_header(mean_mat, std_dev_mat, file_to_copy_header,
+                              export_filename, skip_header=0, skip_footer=0):
+    with open(file_to_copy_header, 'r') as f:
+        lines = f.readlines()
+        header = lines[:skip_header]
+        lines = lines[skip_header:len(lines) - skip_footer]
+        first_column = [l.split(';')[0] for l in lines]
+    with open(export_filename, 'w') as f:
+        if header:
+            f.writelines(header)  # copy header
+        for i in range(len(first_column)):
+            if len(mean_mat.shape) == 1:
+                mean_stddev = [("{:.3f}".format(mean_mat[i]),
+                                "{:.3f}".format(std_dev_mat[i]))]
+            else:
+                mean_stddev = zip(map("{:.3f}".format, mean_mat[i]),
+                                  map("{:.3f}".format, std_dev_mat[i]))
+            mean_mat_line = "; ".join([" +- ".join(m) for m in mean_stddev])
+            f.write(first_column[i] + "; " + mean_mat_line + "; \n")
+
+def evaluate_mean():
+    confusion_mats = []
+    accuracy_mats = []
+    for i in range(1):
+        confusion_mat_path = "../results/{0}/confusion_matrix.csv".format(i)
+        accuracy_mat_path = "../results/{0}/accuracy.csv".format(i)
+        confusion_mats.append(np.genfromtxt(confusion_mat_path,
+                                            usecols=list(range(1, 5)),
+                                            delimiter=';', skip_header=1,
+                                            skip_footer=1))
+        accuracy_mats.append(np.genfromtxt(accuracy_mat_path,
+                                           usecols=(1,),
+                                           delimiter=';'))
+    export_np_mat_with_header(np.mean(confusion_mats, axis=0),
+                              np.std(confusion_mats, axis=0),
+                              "../results/0/confusion_matrix.csv",
+                              "../results/mean_confusion_matrix_testset.csv", 1, 1)
+    export_np_mat_with_header(np.mean(accuracy_mats, axis=0),
+                              np.std(accuracy_mats, axis=0),
+                              "../results/0/accuracy.csv",
+                              "../results/mean_accuracy_matrix_testset.csv")
+
+
+if __name__ == "__main__":
+    for i in range(1):  # train the 5 splits
+        cwd = os.getcwd()
+        storage_dir = "../results/{0}/snapshots/".format(i)
+        evaluate_results(i)
+    evaluate_mean()
